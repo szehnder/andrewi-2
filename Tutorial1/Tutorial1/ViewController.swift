67ddee328713da0764da47e4bab927785b10313f
Sean Zehnder <sean@illcorporation.com>
2014-08-25 13:16:22 -0400

    initial commit

---
+++ Tutorial1/Tutorial1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tutorial1
+//
+//  Created by Sean Zehnder on 8/25/14.
+//  Copyright (c) 2014 Ill Corporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
